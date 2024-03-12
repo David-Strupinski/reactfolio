18fdd191fd63de342b93d06a59daf4581aa8d3c9
David Strupinski <david.strupinski@gmail.com>
2024-03-11 23:13:13 -0700

    add new icons with credit

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -10,16 +10,19 @@ const Footer = () => {
 				<div className="footer-links">
 					<ul className="footer-nav-link-list">
 						<li className="footer-nav-link-item">
-							<Link to="/">Home</Link>
+							<a href="https://www.flaticon.com/free-icons/react" title="react icons">React icons created by Freepik - Flaticon</a>
 						</li>
 						<li className="footer-nav-link-item">
-							<Link to="/about">About</Link>
+							<a href="https://www.flaticon.com/free-icons/django" title="django icons">Django icons created by Freepik - Flaticon</a>
 						</li>
 						<li className="footer-nav-link-item">
-							<Link to="/projects">Projects</Link>
+							<a href="https://www.flaticon.com/free-icons/postgresql" title="postgresql icons">Postgresql icons created by LAFS - Flaticon</a>
 						</li>
 						<li className="footer-nav-link-item">
-							<Link to="/contact">Contact</Link>
+							<a href="https://www.flaticon.com/free-icons/vue" title="vue icons">Vue icons created by JunGSa - Flaticon</a>
+						</li>
+						<li className="footer-nav-link-item">
+							<a href="https://www.flaticon.com/free-icons/gnu-bash" title="gnu bash icons">Gnu bash icons created by Freepik - Flaticon</a>
 						</li>
 					</ul>
 				</div>
