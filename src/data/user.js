999e68184e11d794ceec1ac03e2a44f30765fcad
David Strupinski <david.strupinski@gmail.com>
2024-03-15 18:06:48 -0700

    improve homepage, about, contact paragraphs

--- src/data/user.js
+++ src/data/user.js
@@ -14,13 +14,13 @@ const INFO = {
 	homepage: {
 		title: "Full-stack developer, amateur photographer.",
 		description:
-			"I am a backend-focused full stack developer with expertise in Django. I have experience in building scalable, secure, and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+			"I am a backend-focused full stack developer with expertise in Django. I have experience in building scalable, secure, and reliable web applications using various frameworks and technologies, including but not limited to Django, React, Docker, and Kubernetes. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
 	},
 
 	about: {
 		title: "I'm David Strupinski. I live in Seattle, where I design the future.",
 		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are public and available for others to explore. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
 
 	projects: [
