4120701fc6805915bff3619964b0f938d58dd42a
David Strupinski <david.strupinski@gmail.com>
2024-03-22 13:51:45 -0700

    add project image

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -37,29 +37,45 @@ const Projects = () => {
 							<Logo width={46} />
 						</div>
 					</div>
-					<div className="projects-container">
-						<div className="title projects-title">
-							Things I've made trying to put my dent in the
-							universe.
-						</div>
+        <div className="projects-container">
+          <div className="project-first-area">
+            <div className="project-first-area-left-side">
+              <div className="title projects-title">
+                Things I've made trying to put my dent in the
+                universe.
+              </div>
 
-						<div className="subtitle projects-subtitle">
-							I've worked on a variety of projects over the years
-							and I'm proud of the progress I've made. Many of
-							these projects are open-source and available for
-							others to explore and contribute to. If you're
-							interested in any of the projects I've worked on,
-							please feel free to check out the code and suggest
-							any improvements or enhancements you might have in
-							mind. Collaborating with others is a great way to
-							learn and grow, and I'm always open to new ideas and
-							feedback.
-						</div>
+              <div className="subtitle projects-subtitle">
+                I've worked on a variety of projects over the years
+                and I'm proud of the progress I've made. Many of
+                these projects are open-source and available for
+                others to explore and contribute to. If you're
+                interested in any of the projects I've worked on,
+                please feel free to check out the code and suggest
+                any improvements or enhancements you might have in
+                mind. Collaborating with others is a great way to
+                learn and grow, and I'm always open to new ideas and
+                feedback.
+              </div>
+            </div>
 
-						<div className="projects-list">
-							<AllProjects />
-						</div>
-					</div>
+            <div className="project-first-area-right-side">
+              <div className="project-image-container">
+                <div className="project-image-wrapper">
+                  <img
+                    src="/reactfolio/project.png"
+                    alt="project"
+                    className="projects-image"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="projects-list">
+            <AllProjects />
+          </div>
+        </div>
 					<div className="page-footer">
 						<Footer />
 					</div>
